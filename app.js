68fdd41f93eeb7b5a3670260f000b676cfdc9215
yvgestel <44677393+yvgestel@users.noreply.github.com>
2021-02-22 20:24:59 +0100

    change app.js

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ mongoose.connect(
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
-    });
+    }).then(() => console.log("MongoDb connected"))
+    .catch(err => console.log(err));
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
